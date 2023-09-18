a356dd783fd5dd9740eeabb7a70e842c5981b639
I U Lakshmi Neelima <92011032+udayaInakonda@users.noreply.github.com>
2023-09-18 14:04:42 +0530

    Update goal.model.ts

--- src/app/goals/goal.model.ts
+++ src/app/goals/goal.model.ts
@@ -2,7 +2,8 @@ export interface Goal {
   goal_name: string;
   goalduration: number;
   goaldistance: number;
-  duration_progress: number;
-  distance_progress: number;
-  deadline:string;
+  duration_progress?: number; // Add a "?" to make duration_progress optional
+  distance_progress?: number;
+  deadline_start:string;
+  deadline_end:string;
 }
